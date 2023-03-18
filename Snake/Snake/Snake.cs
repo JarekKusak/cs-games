fdeb692f10403eeb7d768463a7062b1bda613b4f
Jarda Kusák <jarekusak@seznam.cz>
2023-03-18 23:52:09 +0100

    změna názvu proměnných a doplnění dokumentace

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -14,31 +14,34 @@ namespace Snake
         private char body;
         public int n;
         public int m;
-        private int[] cx; // pole ukládající souřadnice x 
-        private int[] cy; // pole ukládající souřadnice y 
-        private int bodySegments; // counter všech článků těla (včetně hlavy) + zmizík na konci 
+        private int[] cx; // x coordinates of body parts
+        private int[] cy; // y coordinates of body parts
+        private int bodyParts; // counter of all body parts (including head and "remover")
         private bool collision;
 
-        public Snake(Table tabulka, Apple jablko, char hlava, char telo)
+        public Snake(Table table, Apple apple, char head, char body)
         {
-            this.head = hlava;
-            this.body = telo;
-            this.apple = jablko;
-            this.table = tabulka;
+            this.head = head;
+            this.body = body;
+            this.apple = apple;
+            this.table = table;
             cx = new int[(table.Length - 2) * (table.Length - 2)];
             cy = new int[(table.Length - 2) * (table.Length - 2)];
             n = (table.Length + 1) / 2 - 1;
             m = n;
             cx[0] = n;
             cy[0] = m;
-            cx[1] = n; // x souř. zmizíku
-            cy[1] = m + 1; // y souř. zmizíku
-            bodySegments = 2; // počet článků těla hada - ze začátku hlava + "zmizík"
+            cx[1] = n; // x coor. of "remover"
+            cy[1] = m + 1; // y coor. of "remover"
+            bodyParts = 2; // number of body segments: head + "remover"
             collision = true;
 
-            apple.CreateApple(cx, cy, bodySegments, m, n); // creates first apple
+            apple.CreateApple(cx, cy, bodyParts, m, n); // creates first apple
         }
 
+        /// <summary>
+        /// Upwards movement of snake
+        /// </summary>
         public void GoUp()
         {
             m--; // coordinates of head for control 
@@ -47,7 +50,7 @@ namespace Snake
                 m = table.Length - 2; // head of snake goes over the edge (upwards)
                 if (apple.CheckStateOfApple(m, n) == false) // false -> snakes moves normally 
                 {
-                    for (int i = bodySegments - 1; i > 0; i--)  
+                    for (int i = bodyParts - 1; i > 0; i--)  
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1]; // swapping y coordinates 
@@ -56,21 +59,21 @@ namespace Snake
                 }
                 else // snake eats apple and its body grows
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cy[0] = table.Length - 2; // displacement of head (upwards)
-                    bodySegments++; // +1 body segment
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++; // +1 body segment
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             else // out of range
             {
                 if (apple.CheckStateOfApple(m, n) == false) // false -> snakes moves normally 
                 {
-                    for (int i = bodySegments - 1; i > 0; i--) 
+                    for (int i = bodyParts - 1; i > 0; i--) 
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1];
@@ -79,19 +82,23 @@ namespace Snake
                 }
                 else // snake eats apple and its body grows 
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cy[0] = cy[0] - 1; // displacement of head (upwards)
-                    bodySegments++; // +1 body segment 
+                    bodyParts++; // +1 body segment 
                     
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             SnakeOutput();
         }
+
+        /// <summary>
+        /// Downwards movement of snake
+        /// </summary>
         public void GoDown()
         {
             m++;
@@ -100,7 +107,7 @@ namespace Snake
                 m = 1;
                 if (apple.CheckStateOfApple(m, n) == false)
                 {
-                    for (int i = bodySegments - 1; i > 0; i--)
+                    for (int i = bodyParts - 1; i > 0; i--)
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1]; 
@@ -109,21 +116,21 @@ namespace Snake
                 }
                 else 
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cy[0] = 1;
-                    bodySegments++;
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++;
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             else // out of range
             {
                 if (apple.CheckStateOfApple(m, n) == false)
                 {
-                    for (int i = bodySegments - 1; i > 0; i--) 
+                    for (int i = bodyParts - 1; i > 0; i--) 
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1];
@@ -132,18 +139,21 @@ namespace Snake
                 }
                 else
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cy[0] = cy[0] + 1;
-                    bodySegments++;
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++;
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             SnakeOutput();
         }
+        /// <summary>
+        /// Left movement of snake
+        /// </summary>
         public void GoLeft()
         {
             n--;
@@ -152,7 +162,7 @@ namespace Snake
                 n = table.Length - 2;
                 if (apple.CheckStateOfApple(m, n) == false)
                 {
-                    for (int i = bodySegments - 1; i > 0; i--) 
+                    for (int i = bodyParts - 1; i > 0; i--) 
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1];
@@ -161,21 +171,21 @@ namespace Snake
                 }
                 else
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cx[0] = table.Length - 2;
-                    bodySegments++;
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++;
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             else
             {
                 if (apple.CheckStateOfApple(m, n) == false)
                 {
-                    for (int i = bodySegments - 1; i > 0; i--)  
+                    for (int i = bodyParts - 1; i > 0; i--)  
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1];
@@ -184,18 +194,21 @@ namespace Snake
                 }
                 else 
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cx[0] = cx[0] - 1;
-                    bodySegments++;
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++;
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             SnakeOutput();
         }
+        /// <summary>
+        /// Right movement of snake
+        /// </summary>
         public void GoRight()
         {
             n++;
@@ -204,7 +217,7 @@ namespace Snake
                 n = 1;
                 if (apple.CheckStateOfApple(m, n) == false)
                 {
-                    for (int i = bodySegments - 1; i > 0; i--)  
+                    for (int i = bodyParts - 1; i > 0; i--)  
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1]; 
@@ -214,21 +227,21 @@ namespace Snake
                 }
                 else 
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cx[0] = 1; 
-                    bodySegments++;
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++;
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             else // out of range
             {
                 if (apple.CheckStateOfApple(m, n) == false)
                 {
-                    for (int i = bodySegments - 1; i > 0; i--) 
+                    for (int i = bodyParts - 1; i > 0; i--) 
                     {
                         cx[i] = cx[i - 1];
                         cy[i] = cy[i - 1];
@@ -237,39 +250,44 @@ namespace Snake
                 }
                 else // snake eats apple and its body grows
                 {
-                    for (int i = bodySegments - 1; i > -1; i--)
+                    for (int i = bodyParts - 1; i > -1; i--)
                     {
                         cx[i + 1] = cx[i];
                         cy[i + 1] = cy[i];
                     }
                     cx[0] = cx[0] + 1; // displacement of head coordinates  
-                    bodySegments++; // +1 segment
-                    apple.CreateApple(cx, cy, bodySegments, m, n);
+                    bodyParts++; // +1 segment
+                    apple.CreateApple(cx, cy, bodyParts, m, n);
                 }
             }
             SnakeOutput();
         }
-
+        /// <summary>
+        /// Outputing snake on the screen
+        /// </summary>
         public void SnakeOutput() // outputs whole snake on new positions
         {
             Console.SetCursorPosition(2 * cx[0], cy[0]);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine(head);
 
-            for (int i = 1; i < bodySegments - 1; i++)
+            for (int i = 1; i < bodyParts - 1; i++)
             {
                 Console.SetCursorPosition(2 * cx[i], cy[i]);
                 Console.WriteLine(body);
             }
 
-            Console.SetCursorPosition(2 * cx[bodySegments - 1], cy[bodySegments - 1]);
+            Console.SetCursorPosition(2 * cx[bodyParts - 1], cy[bodyParts - 1]);
             Console.WriteLine(table.Character); // deletes last character
             Console.ResetColor();
         }
-
+        /// <summary>
+        /// Checks if snake collided with his body
+        /// </summary>
+        /// <returns> did/did not </returns>
         public bool CheckCollision() // checks if collision happened
         {
-            for (int i = 1; i < bodySegments - 1; i++) // i = 1, because head is not counted
+            for (int i = 1; i < bodyParts - 1; i++) // i = 1, because head is not counted
             {
                 if (n == cx[i] && m == cy[i])
                 {
