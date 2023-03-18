39344e39b12bf313cb84f180dafa401606608683
Jarda Kusák <jarekusak@seznam.cz>
2023-03-18 20:42:21 +0100

    Přidání třídy Game + rozvržení základních funkcí hry

--- Snake/Snake/Game.cs
+++ Snake/Snake/Game.cs
@@ -8,5 +8,73 @@ namespace Snake
 {
     internal class Game
     {
+        private Table table;
+        private Snake snake;
+        private Apple apple;
+        private ConsoleKeyInfo c; // třída s potřebnými metodami pro kontrolu vstupu uživatele
+        private bool gameStillGoing;
+        private bool playMore;
+        private char move;
+        private int doba;
+
+        public Game(Table tabulka, int doba)
+        {
+            c = new ConsoleKeyInfo();
+            playMore = true;
+            this.table = tabulka;
+            this.doba = doba;
+        }
+
+        public bool Play() // metoda spustí hru 
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            // založení nových objektů                
+            apple = new Apple(table, 'O');
+            snake = new Snake(table, apple, 'X', 'x');
+            // začátek hry 
+            table.TableOutput();
+            gameStillGoing = true;
+             
+            Console.SetCursorPosition(1, table.Length);
+            move = Console.ReadKey().KeyChar; // požadavek na zahájení tahu 
+            while (gameStillGoing)
+            {
+                //Tah();
+            }
+            playMore = Question();
+            Console.Clear();
+            return playMore;
+        }
+        public bool Question() // metoda se zeptá hráče na založení nové hry
+        {
+            Console.SetCursorPosition(1, table.Length);
+            Console.WriteLine("Přejete si založit novou hru? [ano/ne]");
+            bool validOption = false;
+            bool pokracovat = true;
+            while (!validOption)
+            {
+                switch (Console.ReadLine().ToString().ToLower())
+                {
+                    case "ano":
+                        pokracovat = true;
+                        validOption = true;
+                        break;
+                    case "ne":
+                        pokracovat = false;
+                        validOption = true;
+                        break;
+                    default:
+                        Console.WriteLine("Neplatná volba, zadejte prosím [ano/ne]");
+                        break;
+                }
+            }
+            return pokracovat;
+        }
+        public bool End() // metoda na ověřování eventuální kolize hada a těla => konce hry 
+        {
+            //gameStillGoing = snake.CheckCollision(); ;
+            return gameStillGoing;
+        }
     }
 }
