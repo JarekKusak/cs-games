d01a043da4093d54bb1257640da4dbd358f16aee
Jarda Kusák <jarekusak@seznam.cz>
2023-04-17 13:26:41 +0200

    rozdělání projekt Pexeso

---
+++ Pexeso/Pexeso/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Pexeso
+{
+    internal class Game : INotifyPropertyChanged
+    {
+        Button[,] buttons;
+        //INotifyPropertyChanged event
+        //INotifyPropertyChanged event
+        public event PropertyChangedEventHandler PropertyChanged;
+        public int Tries { get; set; }
+        public int Pairs { get; set; }
+        public Game(Button[,] buttons, int pairs) 
+        {
+            this.buttons = buttons;
+            Pairs = pairs;
+            Tries = 0;
+            //OnPropertyChanged(nameof(Pairs));
+        }
+        /// <summary>
+        /// Aktualizace TextBlocků
+        /// </summary>
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Pexeso/Pexeso/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace Pexeso
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int n;
+        private int m;
+        private int pairs;
+        private Button[,] btns;
+        public MainWindow()
+        {
+            InitializeComponent();
+                
+            btns = new Button[n, m];
+            n = 4;
+            m = 5;
+            pairs = (n * m)/2; // 10 pairs
+            Loaded += LoadBoard;
+            Loaded += NewGame;
+            restartBtn.Click += NewGame;
+        }
+
+        private void NewGame(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game(btns, pairs);
+            DataContext = game;
+        }
+
+        private void LoadBoard(object sender, RoutedEventArgs e)
+        {
+            board.HorizontalAlignment = HorizontalAlignment.Stretch;
+            board.VerticalAlignment = VerticalAlignment.Stretch;
+            
+            // nastavení gridu
+            for (int i = 0; i < n; i++)
+            {
+                var rd = new RowDefinition();
+                rd.Height = new GridLength(1, GridUnitType.Star);
+                board.RowDefinitions.Add(rd);
+                
+            }
+            for (int j = 0; j < m; j++)
+            {
+                var cd = new ColumnDefinition();
+                cd.Width = new GridLength(1, GridUnitType.Star);
+                board.ColumnDefinitions.Add(cd);
+            }
+
+
+            // vyplnění gridu buttony
+            for (int j = 0; j < m; j++)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Border border = new Border();
+                    border.BorderThickness = new Thickness(1);
+                    border.BorderBrush = new SolidColorBrush(Colors.Black);
+                    border.HorizontalAlignment = HorizontalAlignment.Stretch;
+                    border.VerticalAlignment = VerticalAlignment.Stretch;
+
+                    Button btn = new Button();
+                    btn.Height = 30;
+                    btn.Width = 30;
+                    btn.FontSize = 5;
+                    btn.Content = string.Format($"i={i}; j={j}");
+                    //btn.Margin = new Thickness(2);
+
+                    Grid.SetColumn(border, j);
+                    Grid.SetRow(border, i);
+                    border.Child = btn;
+
+                    board.Children.Add(border);
+                }
+            }
+        }
+    }
+}
